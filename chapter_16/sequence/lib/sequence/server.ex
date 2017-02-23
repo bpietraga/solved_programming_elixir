44c206d2a29df22113a5673f9063e4d2af2b1f24
Bernard Pietraga <bernard.pietraga@gmail.com>
2017-02-23 20:27:25 +0100

    Add exercise 2 from chapter 16 and small indentation fixes to no 1

--- chapter_16/sequence/lib/sequence/server.ex
+++ chapter_16/sequence/lib/sequence/server.ex
@@ -4,8 +4,11 @@ defmodule Sequence.Server do
   def handle_call({:set_stack, stack}, _from, _stack), do:
     {:reply, stack, stack}
 
-  def handle_call(:pop, _from, []),
-    do: {:reply, [], []}
-  def handle_call(:pop, _from, [head | tail]),
-    do: {:reply, head, tail}
+  def handle_call(:pop, _from, []), do:
+    {:reply, [], []}
+  def handle_call(:pop, _from, [head | tail]), do:
+    {:reply, head, tail}
+
+  def handle_cast({:push, element}, list), do:
+    {:noreply, [element | list]}
 end
--- chapter_16/sequence/test/sequence_server_test.exs
+++ chapter_16/sequence/test/sequence_server_test.exs
@@ -3,7 +3,7 @@ defmodule SequenceServerTest do
   doctest Sequence
 
   test "pop removes first element (not like normal pop)" do
-    {:ok, pid} = GenServer.start_link(Sequence.Server, [5,"cat",9])
+    {:ok, pid} = GenServer.start_link(Sequence.Server, [5, "cat", 9])
     expected = GenServer.call(pid, :pop)
     second_expected = GenServer.call(pid, :pop)
     third_expected = GenServer.call(pid, :pop)
@@ -13,6 +13,15 @@ defmodule SequenceServerTest do
     assert second_expected == "cat"
     assert third_expected  == 9
     assert fourth_expected == []
+  end
+
+
+  test "push add element to array" do
+    {:ok, pid} = GenServer.start_link(Sequence.Server, [1, 2, 3])
+    GenServer.cast(pid, {:push, "mayoness"})
+    expected = GenServer.call(pid, :pop)
+
+    assert expected == "mayoness"
   end
 
   test "set_stack does its job" do
